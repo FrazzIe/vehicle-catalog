d838879af2fe1bf278c8fe7aaf94b4cd1eb0f758
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-11-20 10:06:16 +0000

    add onclick handler for categories

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import { data } from "./data.js";
 
 let app = document.getElementById("app");
+let selectedCategory;
 
 function show(val) {
 	let show = val === true;
@@ -8,6 +9,21 @@ function show(val) {
 		app.style.display = "initial";
 	else
 		app.style.display = "none";
+}
+
+function onCategoryClicked(event) {
+	let target = event.target;
+	let idx = target.dataset.idx;
+
+	if (data.vehicles.length <= idx)
+		return;
+
+	if (selectedCategory != null) {
+		selectedCategory.classList.remove("class-selected");
+	}
+
+	selectedCategory = target;
+	selectedCategory.classList.add("class-selected");
 }
 
 function populateCategories() {
@@ -22,8 +38,10 @@ function populateCategories() {
 	item.classList.add("class-item", "class-selected");
 	item.textContent = data.categories[0];
 	item.dataset.idx = 0;
+	item.onclick = onCategoryClicked;
 
 	container.appendChild(item);
+	selectedCategory = item;
 
 	for (let i = 1; i < numCategories; i++) {
 		let item = document.createElement("button");
@@ -31,9 +49,10 @@ function populateCategories() {
 		item.classList.add("class-item");
 		item.textContent = data.categories[i];
 		item.dataset.idx = i;
+		item.onclick = onCategoryClicked;
 
 		container.appendChild(item);
-	}	
+	}
 }
 
 function init() {
