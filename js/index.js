8cb1b4d72ca06d53c15a7a2781c3417c45229bc5
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-11-20 09:39:20 +0000

    add populateCategories func

--- js/index.js
+++ js/index.js
@@ -10,8 +10,33 @@ function show(val) {
 		app.style.display = "none";
 }
 
+function populateCategories() {
+	let container = app.querySelector(".class-container");
+	let numCategories = data.categories.length;
+
+	if (numCategories == 0)
+		return;
+	
+	let item = document.createElement("button");
+
+	item.classList.add("class-item", "class-selected");
+	item.textContent = data.categories[0];
+
+	container.appendChild(item);
+
+	for (let i = 1; i < numCategories; i++) {
+		let item = document.createElement("button");
+
+		item.classList.add("class-item");
+		item.textContent = data.categories[i];
+
+		container.appendChild(item);
+	}	
+}
+
 function init() {
 	// show(false);
+	populateCategories();
 }
 
 init();
