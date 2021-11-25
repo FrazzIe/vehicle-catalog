d34004f4e696556ba8e239df9068ab35ebb61f64
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-11-25 12:34:39 +0000

    add dataURItoBlob func
    https://stackoverflow.com/a/12300351

--- ui/js/screenshot.js
+++ ui/js/screenshot.js
@@ -2,6 +2,22 @@ import { GameRender } from "./gameview.js";
 
 const resourceName = "GetParentResourceName" in window ? GetParentResourceName() : "unknown";
 
+// from https://stackoverflow.com/a/12300351
+function dataURItoBlob(dataURI) {
+    const byteString = atob(dataURI.split(',')[1]);
+    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+  
+    for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+  
+    const blob = new Blob([ab], { type: mimeString });
+    return blob;
+}
+
 function setupVehicleForImage(vehicle) {
 	return fetch(`https://${resourceName}/setupImage`, {
 		method: "POST",
