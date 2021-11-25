34392824192cbbb5b85c6317336bdbe509f076b3
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-11-25 10:29:45 +0000

    add handler for capturing and uploading vehicle imgs

---
+++ ui/js/screenshot.js
@@ -0,0 +1,43 @@
+import { GameRender } from "./gameview.js";
+
+const resourceName = "GetParentResourceName" in window ? GetParentResourceName() : "unknown";
+
+async function setupVehicleForImage(vehicle) {
+	const result = await fetch(`http://${resourceName}/setupImage`, {
+		method: "POST",
+		body: JSON.stringify(vehicle)
+	}).then(function(response) {
+		return response.json();
+	}).then(function(data) {
+		return data === "ok";
+	});
+
+	return result;
+}
+
+function captureVehicleImage(gameView, vehicle) {
+	const imageURL = gameView.canvas.toDataURL("image/png", 0.92);
+	const formData = new FormData();
+
+	formData.append("name", vehicle.model);
+	formData.append("file", dataURItoBlob(imageURL));
+
+	// upload somewhere
+}
+
+function generateVehicleImages(data) {
+	let gameView = GameRender();
+
+	for (let i = 0; i < data.length; i++) {
+		for (let j = 0; j < data[i].length; j++) {
+			let isReady = setupVehicleForImage(data[i][j]);
+
+			if (isReady)
+				captureVehicleImage(gameView, vehicle);
+		}
+	}
+
+	gameView.animationFrame = void 0;
+}
+
+export { generateVehicleImages }
