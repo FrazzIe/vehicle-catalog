d2ddd12b5a143e2dce16bdd97ac2a83225c971d6
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-11-26 10:25:26 +0000

    add auth check to handler

--- http/upload.js
+++ http/upload.js
@@ -7,24 +7,43 @@ const acceptedMimetypes = {
 	"image/jpeg": ".jpeg",
 }
 const outputFolder = "generated_images";
+const accessControl = {};
 
 function handler(req, res) {
-	if (path.dirname(req.path) != "/upload") {		
+	req.path = path.normalize(req.path);
+	const pathParams = req.path.split(path.sep);
+
+	if (pathParams.length != 4) {
+		res.writeHead(400);
+		res.send("Invalid route");
+		return;
+	}
+
+	if (pathParams[0] != "" && pathParams[1] != "upload") {		
 		res.writeHead(404);
 		res.send("Not found.");
+		return;
 	}
 
 	if (req.method != "POST") {
 		res.writeHead(405);
 		res.send(`The "${req.method}" method is not allowed`);
+		return;
+	}
+
+	if (!accessControl[pathParams[2]]) {
+		res.writeHead(401);
+		res.send("401 Unauthorized");
+		return;
 	}
 	
 	if (!req.headers["Content-Type"] || !acceptedMimetypes[req.headers["Content-Type"]]) {
 		res.writeHead(415);
 		res.send(`Unsupported media type: ${req.headers["Content-Type"]}`);
+		return;
 	}
 	
-	let fileName = path.basename(req.path);
+	let fileName = pathParams[3];
 	let fileExt = acceptedMimetypes[req.headers["Content-Type"]];
 	let filePath = path.join(GetResourcePath(GetCurrentResourceName()), outputFolder, fileName + fileExt);
 
