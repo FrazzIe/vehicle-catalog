cbb0b5a886782d261b477f1a6fd124d076332dde
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-11-27 15:41:38 +0000

    add checkData func for validation on onOpen param

--- script/cl_catalog.js
+++ script/cl_catalog.js
@@ -1,5 +1,35 @@
 function init() {
-	function onOpen() {
+	let data;
+
+	function checkData(_data) {
+		if (!_data)
+			return [false, "No data received"];
+		
+		if (!_data.camera)
+			return [false, "No camera data received"];
+		
+		if (!_data.camera.pos || isNaN(_data.camera.pos.x) || isNaN(_data.camera.pos.y) || isNaN(_data.camera.pos.z))
+			return [false, "No camera position recieved"];
+
+		if (!_data.camera.rot || isNaN(_data.camera.rot.x) || isNaN(_data.camera.rot.y) || isNaN(_data.camera.rot.z))
+			return [false, "No camera rotation received"];
+
+		if (!_data.vehicle || isNaN(_data.vehicle.x) || isNaN(_data.vehicle.y) || isNaN(_data.vehicle.z) || isNaN(_data.vehicle.w))
+			return [false, "No vehicle position received"];
+
+		data = _data;
+
+		return [true];
+	}
+
+	function onOpen(_data) {
+		let [success, err] = checkData(_data);
+
+		if (!success) {
+			console.log("%s, unable to open.", err);
+			return;
+		}
+
 		SendNuiMessage(JSON.stringify({
 			type: "Show",
 			payload: true
