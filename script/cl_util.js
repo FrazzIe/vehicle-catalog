4a01e460330f0ac843b02541cc051faf20442c7f
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-11-28 11:28:30 +0000

    fix using invalid var

--- script/cl_util.js
+++ script/cl_util.js
@@ -5,10 +5,10 @@ function delay(ms) {
 function setupCamera(data) {
 	let handle = CreateCam("DEFAULT_SCRIPTED_CAMERA", true);
 
-	SetCamCoord(cameraHandle, data.pos.x, data.pos.y, data.pos.z);
-	SetCamRot(cameraHandle, data.rot.x, data.rot.y, data.rot.z, 2);
-	SetCamFov(cameraHandle, 45.0);
-	SetCamActive(cameraHandle, true);
+	SetCamCoord(handle, data.pos.x, data.pos.y, data.pos.z);
+	SetCamRot(handle, data.rot.x, data.rot.y, data.rot.z, 2);
+	SetCamFov(handle, 45.0);
+	SetCamActive(handle, true);
 	RenderScriptCams(true, false, 0, 1, 0);
 
 	return handle;
