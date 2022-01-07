23331376bb21f5faae6ad1035c0ebe36e20c0c0a
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-07 23:52:42 +0000

    feat(ui/stats): update func for bars
    Update vehicle statstics bars

--- ui/js/stats.js
+++ ui/js/stats.js
@@ -1,4 +1,45 @@
 /**
+ * Update vehicle statstics bars
+ * @param {number[]} values Array of bar percentages
+ */
+function update(values) {
+	const stats = document.querySelector(".stats-container");
+
+	for (let i = 0; i < values.length; i++) {
+		const stat = stats.children[i];
+		const bars = stat.children[1];
+
+		const numBars = bars.children.length;
+		const oneFilled = (100.0 / numBars);
+		const floatFilled = values[i] / oneFilled;
+		const numFilled = Math.trunc(floatFilled);
+		const partialFilled = floatFilled % 1;
+		const numEmpty = Math.trunc(numBars - floatFilled);
+
+		// skip if bar is empty
+		if (numEmpty != numBars) {
+			// set filled bars
+			for (let j = 0; j < numFilled; j++) {
+				bars.children[j].children[0].style.width = "100%";
+			}
+			
+			// skip if not partial is needed
+			if (numFilled + numEmpty != numBars) {
+				// set partially filled bar
+				if (bars.children[numFilled] != null) {
+					bars.children[numFilled].children[0].style.width = `${Math.trunc(partialFilled * 100.0)}%`;
+				}
+			}
+		}
+		
+		// set empty bars
+		for (let j = numBars - numEmpty; j < numEmpty; j++) {
+			bars.children[j].children[0].style.width = "0%";
+		}
+	}
+}
+
+/**
  * Initialise vehicle statistics pane
  * @param {string[]} labels Array of resolved stat labels
  */
