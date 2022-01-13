2f5aa5515c7a603ff538ece877665f6dc5eede6d
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-13 13:53:49 +0000

    feat(ui/navbar): get nav element

--- ui/js/navbar.js
+++ ui/js/navbar.js
@@ -3,6 +3,12 @@
  * @param {string} id nav element id
  * @param {string[]} values collection of nav items
  */
-function populate(id, values) {
+function populate(id, values) 
+{
+	if (id == null)
+	{
+		throw "nav element id is null";
+	}
 
+	const nav = document.getElementById(id);
 }
