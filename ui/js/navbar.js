038217faa263fd0d5a325ad8981069a000c8e446
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-13 14:27:48 +0000

    feat(ui/navbar): optional empty param
    - removes all child elements from parent

--- ui/js/navbar.js
+++ ui/js/navbar.js
@@ -2,8 +2,9 @@
  * Add items to navbar
  * @param {string} id nav element id
  * @param {string[]} values collection of nav items
+ * @param {bool} [empty] empty previous nav items
  */
-function populate(id, values) 
+function populate(id, values, empty = true) 
 {
 	if (id == null)
 	{
@@ -20,5 +21,14 @@ function populate(id, values)
 	if (values == null || values.length == 0)
 	{
 		return;
+	}
+
+	if (empty == true)
+	{
+		// remove all child elements from nav
+		while (nav.firstChild != null)
+		{
+			nav.removeChild(nav.lastChild);
+		}
 	}
 }
