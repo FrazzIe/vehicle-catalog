b90721a1131c35445a2f507b59625231ebb7e015
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-13 12:55:38 +0000

    feat(ui): onMessage func handler

--- ui/js/index.js
+++ ui/js/index.js
@@ -1,5 +1,21 @@
-function init() {
+/**
+ * Listen for messages received from game script
+ * @param {Event} event 
+ */
+function onMessage(event)
+{
+	const data = event.data || event.detail;
+}
 
+/**
+ * Initilaise globals
+ * 
+ * Add event listeners
+ */
+function init()
+{
+	// listen for events
+	window.addEventListener("message", onMessage);
 }
 
 init();
