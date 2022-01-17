37463ff9cc331f60fb0be70a8807951cc63f44e5
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-17 17:44:59 +0000

    feat(ui/slider): empty funcs

---
+++ ui/js/slider.js
@@ -0,0 +1,48 @@
+/**
+ * Event Listener for slider item clicks
+ * @param {Event} event event object
+ */
+function onClick(event)
+{
+
+}
+
+/**
+ * Add item to slider
+ */
+function add()
+{
+
+}
+
+/**
+ * Add items to slider
+ * @param {string} id slider element id
+ * @param {object[]} values collection of slider items
+ * @param {bool} [empty] empty previous slider items
+ */
+function populate(id, values, empty = true) 
+{
+
+}
+
+/**
+ * Create a slider
+ * @param {string} id slider element id
+ * @param {HTMLElement} parent parent element to append slider
+ * @param {object[]} [values] collection of slider items
+ */
+function create(id, parent, values)
+{
+
+}
+
+/**
+ * Add/Subtract selected item index
+ * @param {string} id slider element id
+ * @param {number} amount amount to increment
+ */
+function increment(id, amount)
+{
+
+}
