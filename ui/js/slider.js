0f68b9d5ffa185bf9249f9673c162ff571a808fe
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-24 22:23:33 +0000

    feat(ui/slider): move func set into Slider class

--- ui/js/slider.js
+++ ui/js/slider.js
@@ -1,481 +1,388 @@
-const DEFAULT_NUM_VISIBLE = 5;
-const indexChangedEvent = "SliderIndexChanged";
-
 /**
- * Set selected item index
- * @param {HTMLDivElement} slider slider element
- * @param {number} newIndex slider item index
- * @param {bool} [delay] delay previous item index
+ * Class representing a slider
  */
-function setIndex(slider, newIndex, delay = false)
+class Slider
 {
-	if (slider == null)
+	static DEFAULT_NUM_VISIBLE = 5;
+	static INDEX_CHANGED_EVENT = "SliderIndexChanged";
+
+	#index;
+	#min;
+	#max;
+	#numVisible;
+
+	/**
+	 * Create a slider
+	 * @param {string} id slider element id
+	 * @param {object[]} [values] collection of slider items
+	 * @param {number} [numVisible] number of items to show by default
+	 */
+	constructor(id, values, numVisible)
 	{
-		throw `slider element is null`;
-	}
-
-	const items = slider.children[1];
-
-	if (items == null)
-	{
-		throw `slider element with id "${slider.id}" doesn't have items container`;
-	}
-
-	let curIndex = parseInt(items.dataset.index);
-
-	if (isNaN(curIndex))
-	{
-		return;
-	}
-
-	// change selected element
-	const last = items.children[curIndex];
-	const next = items.children[newIndex];
-
-	if (last != null)
-	{
-		last.classList.remove("selected");
-
-		if (delay)
+		if (id == null)
 		{
-			last.classList.add("previous");
+			throw "slider element id is null";
+		}
 
-			setTimeout(function()
+		this.#numVisible = parseInt(numVisible);
+
+		if (isNaN(this.#numVisible) || this.#numVisible <= 0)
+		{
+			this.#numVisible = Slider.DEFAULT_NUM_VISIBLE;
+		}
+
+		this.domElement = document.createElement("div");
+
+		this.domElement.id = id;
+		this.domElement.classList.add("slider");
+
+		// left arrow
+		this.#arrow(true);
+
+		// items container
+		const items = document.createElement("div");
+
+		items.id = `${this.domElement.id}-items`;
+		items.classList.add("item-container");
+		items.dataset.visible = this.#numVisible;
+
+		this.domElement.appendChild(items);
+
+		// right arrow
+		this.#arrow(false);
+
+		this.populate(values);
+	}
+
+	/**
+	 * Create a slider arrow
+	 * @param {bool} left arrow direction
+	 */
+	#arrow(left)
+	{
+		let direction;
+		let amount;
+	
+		if (left)
+		{
+			direction = "left";
+			amount = -1;
+		}
+		else
+		{
+			direction = "right";
+			amount = 1;
+		}
+
+		// create arrow container
+		const container = document.createElement("div");
+
+		container.id = `${this.domElement.id}-arrow-${direction}`;
+		container.classList.add("arrow-container");
+
+		// create arrow
+		// append to container
+		const arrow = document.createElement("div");
+
+		arrow.classList.add("arrow", direction);
+
+		container.appendChild(arrow);
+
+		// container onclick
+		container.onclick = () =>
+		{
+			this.change(amount);
+		};
+
+		// append to slider
+		this.domElement.appendChild(container);
+	}
+
+	/**
+	 * Set selected item index
+	 * @param {number} idx slider item index
+	 * @param {bool} [delay] delay previous item index
+	 */
+	#setIndex(idx, delay = false)
+	{
+		const items = this.domElement.children[1];
+	
+		if (items == null)
+		{
+			throw `slider element with id "${this.domElement.id}" doesn't have items container`;
+		}	
+
+		// change selected element
+		const last = items.children[this.#index];
+		const next = items.children[idx];
+	
+		if (last != null)
+		{
+			last.classList.remove("selected");
+	
+			if (delay)
 			{
-				last.classList.remove("previous");
-			}, 20);
+				last.classList.add("previous");
+	
+				setTimeout(() => last.classList.remove("previous"), 20);
+			}
 		}
-	}
-
-	if (next != null)
-	{
-		next.classList.add("selected");
-	}
-
-	// store new index
-	items.dataset.index = newIndex;
-
-	const customEvent = new CustomEvent(indexChangedEvent, {
-		detail: {
-			old: curIndex,
-			new: newIndex
+	
+		if (next != null)
+		{
+			next.classList.add("selected");
 		}
-	});
+	
+		const customEvent = new CustomEvent(Slider.INDEX_CHANGED_EVENT,
+		{
+			detail: {
+				old: this.#index,
+				new: idx
+			}
+		});
 
-	slider.dispatchEvent(customEvent);
-}
+		// store new index
+		this.#index = idx;
+		items.dataset.index = idx;
 
-/**
- * Add item to slider
- * @param {HTMLDivElement} slider slider element
- * @param {object} data slider item data
- * @param {string} data.model vehicle model
- * @param {number} [data.price] vehicle price
- * @param {number} idx slider item index
- */
-function add(slider, data, idx)
-{
-	if (slider == null)
-	{
-		throw `slider element is null`;
+		this.domElement.dispatchEvent(customEvent);
 	}
 
-	const items = slider.children[1];
-
-	if (items == null)
+	/**
+	 * Add item to slider
+	 * @param {object} data slider item data
+	 * @param {string} data.model vehicle model
+	 * @param {number} [data.price] vehicle price
+	 * @param {number} idx slider item index
+	 */
+	#add(data, idx)
 	{
-		throw `slider element with id "${slider.id}" doesn't have items container`;
-	}
+		const items = this.domElement.children[1];
 
-	if (idx == null)
-	{
-		throw "slider item index is null";
-	}
+		if (items == null)
+		{
+			throw `slider element with id "${this.domElement.id}" doesn't have items container`;
+		}
 
-	// create item
-	const item = document.createElement("div");
+		if (idx == null)
+		{
+			throw "slider item index is null";
+		}
 
-	item.classList.add("item");
-	item.id = `${items.id}-${idx}`;
+		// create item
+		const item = document.createElement("div");
 
-	item.onclick = function()
-	{
-		setIndex(slider, idx);
-	};
+		item.classList.add("item");
+		item.id = `${items.id}-${idx}`;
 
-	// create image for item
-	const image = document.createElement("img");
+		item.onclick = () =>
+		{
+			this.#setIndex(idx);
+		};
 
-	// load default image on error
-	image.onerror = function()
-	{
-		image.onerror = null;
+		// create image for item
+		const image = document.createElement("img");
+
+		// load default image on error
+		image.onerror = function()
+		{
+			image.onerror = null;
+			image.src = "img/default.png";
+		};
+
+		// prevent image drag
+		image.ondragstart = function()
+		{
+			return false;
+		};
+
 		image.src = "";
-	};
 
-	// prevent image drag
-	image.ondragstart = function()
-	{
-		return false;
-	};
+		item.appendChild(image);
 
-	image.src = "";
+		// create text label for item
+		const textLabel = document.createElement("div");
 
-	item.appendChild(image);
+		textLabel.classList.add("name");
+		textLabel.textContent = data.model;
 
-	// create text label for item
-	const textLabel = document.createElement("div");
+		item.appendChild(textLabel);
 
-	textLabel.classList.add("name");
-	textLabel.textContent = data.model;
+		// create price label for item
+		// TODO: optional check
+		const priceLabel = document.createElement("div");
 
-	item.appendChild(textLabel);
+		priceLabel.classList.add("price");
+		priceLabel.textContent = data.price;
 
-	// create price label for item
-	// TODO: optional check
-	const priceLabel = document.createElement("div");
+		item.appendChild(priceLabel);
 
-	priceLabel.classList.add("price");
-	priceLabel.textContent = data.price;
-
-	item.appendChild(priceLabel);
-
-	items.appendChild(item);
-}
-
-/**
- * Add items to slider
- * @param {string} id slider element id
- * @param {object[]} values collection of slider items
- * @param {bool} [empty] empty previous slider items
- */
-function populate(id, values, empty = true) 
-{
-	if (values == null || values.length == 0)
-	{
-		return;
+		items.appendChild(item);	
 	}
 
-	if (id == null)
+	/**
+	 * Add items to slider
+	 * @param {object[]} values collection of slider items
+	 */
+	populate(values) 
 	{
-		throw "slider element id is null";
-	}
+		if (values == null || values.length == 0)
+		{
+			return;
+		}
 
-	const slider = document.getElementById(id);
+		const items = this.domElement.children[1];
 
-	if (slider == null)
-	{
-		throw `slider element with id "${id}" doesn't exist`;
-	}
+		if (items == null)
+		{
+			throw `slider element with id "${this.domElement.id}" doesn't have items container`;
+		}
 
-	const items = slider.children[1];
-
-	if (items == null)
-	{
-		throw `slider element with id "${id}" doesn't have items container`;
-	}
-
-	if (empty == true)
-	{
 		// remove all child elements from items container
 		while (items.firstChild != null)
 		{
 			items.removeChild(items.lastChild);
 		}
-	}
 
-	// add items
-	for (let i = 0; i < values.length; i++)
-	{
-		add(slider, values[i], i);
-	}
+		// add items
+		for (let i = 0; i < values.length; i++)
+		{
+			this.#add(values[i], i);
+		}
 
-	let numVisible = parseInt(items.dataset.visible);
-
-	if (isNaN(numVisible))
-	{
-		numVisible = DEFAULT_NUM_VISIBLE;
-	}
-
-	// add selected class to first child
-	// only if list was emptied
-	if (empty == true)
-	{
-		items.firstChild.classList.add("selected");
-		items.dataset.index = 0;
-		items.dataset.min = 0;
+		this.#index = 0;
+		this.#min = 0;
 
 		// set max
-		if (items.children.length < numVisible)
+		if (items.children.length < this.#numVisible)
 		{
-			items.dataset.max = items.children.length - 1;
+			this.#max = items.children.length - 1;
 		}
 		else
 		{
-			items.dataset.max = numVisible - 1;
+			this.#max = this.#numVisible - 1;
 		}
 
+		// add selected class to first child
+		items.firstChild.classList.add("selected");
+
+		// set element data attributes
+		items.dataset.index = this.#index;
+		items.dataset.min = this.#min;
+		items.dataset.max = this.#max;
+
 		// show visible items
-		for (let i = items.dataset.min; i <= items.dataset.max; i++)
+		for (let i = this.#min; i <= this.#max; i++)
 		{
 			items.children[i].classList.add("visible");
 		}
 	}
+
+	/**
+	 * Add/Subtract selected item index
+	 * @param {number} amount amount to change
+	 */
+	change(amount)
+	{
+		// don't accept 0
+		if (amount == 0)
+		{
+			return;
+		}
+
+		const items = this.domElement.children[1];
+
+		if (items == null)
+		{
+			throw `slider element with id "${this.domElement.id}" doesn't have items container`;
+		}
+
+		const numItems = items.children.length;
+		const increment = Math.abs(amount) % numItems;
+		
+		let newIndex;
+		let newMinIndex = this.#min;
+		let newMaxIndex = this.#max;
+
+		// calc new index
+		if (amount > 0)
+		{
+			newIndex = this.#index + increment;
+
+			// calc new max & min
+			if (newIndex > this.#max)
+			{
+				newMaxIndex = newIndex;
+				newMinIndex = this.#min + (newMaxIndex - this.#max);
+			}
+		}
+		else
+		{
+			newIndex = this.#index - increment;
+
+			if (newIndex < this.#min)
+			{
+				newMinIndex = newIndex;
+				newMaxIndex = this.#max - (this.#min - newMinIndex);
+			}
+		}
+
+		// clamp index in between 0 and numItems
+		if (newIndex >= numItems)
+		{
+			newIndex -= numItems;
+			newMaxIndex -= numItems;
+			newMinIndex -= numItems;
+
+			// prevent min being negative
+			if (newMinIndex < 0)
+			{
+				newMaxIndex -= newMinIndex;
+				newMinIndex -= newMinIndex;
+			}
+		}
+		else if (newIndex < 0)
+		{
+			newIndex = numItems + newIndex;
+			newMinIndex = numItems + newMinIndex;
+			newMaxIndex = numItems + newMaxIndex;
+
+			// prevent max being greater than numItems
+			if (newMaxIndex >= numItems)
+			{
+				newMinIndex -= newMaxIndex - newIndex;
+				newMaxIndex = newIndex;
+			}
+		}
+		
+		let useDelay = false;
+
+		// change item visibility
+		if (this.#min != newMinIndex || this.#max != newMaxIndex)
+		{
+			// hide visible items
+			for (let i = this.#min; i <= this.#max; i++)
+			{
+				items.children[i].classList.remove("visible");
+			}
+
+			// show visible items
+			for (let i = newMinIndex; i <= newMaxIndex; i++)
+			{
+				items.children[i].classList.add("visible");
+			}
+
+			// set min + max
+			this.#min = newMinIndex;
+			this.#max = newMaxIndex;
+
+			// set element data attributes
+			items.dataset.min = newMinIndex;
+			items.dataset.max = newMaxIndex;
+
+			useDelay = true;
+		}
+
+		this.#setIndex(newIndex, useDelay);
+	}
 }
-
-/**
- * Create a slider arrow
- * @param {HTMLDivElement} slider slider element
- * @param {bool} left arrow direction
- */
-function createArrow(slider, left)
-{
-	if (slider == null)
-	{
-		throw `slider element is null`;
-	}
-
-	let direction;
-	let amount;
-
-	if (left == true)
-	{
-		direction = "left";
-		amount = -1;
-	}
-	else
-	{
-		direction = "right";
-		amount = 1;
-	}
-
-	// create arrow container
-	const container = document.createElement("div");
-
-	container.id = `${slider.id}-arrow-${direction}`;
-	container.classList.add("arrow-container");
-
-	// create arrow
-	// append to container
-	const arrow = document.createElement("div");
-
-	arrow.classList.add("arrow", direction);
-
-	container.appendChild(arrow);
-
-	// container onclick
-	container.onclick = function()
-	{
-		change(slider.id, amount);
-	};
-
-	// append to slider
-	slider.appendChild(container);
-}
-
-/**
- * Create a slider
- * @param {string} id slider element id
- * @param {HTMLElement} parent parent element to append slider
- * @param {object[]} [values] collection of slider items
- * @param {number} [numVisible] number of items to show by default
- */
-function create(id, parent, values, numVisible = DEFAULT_NUM_VISIBLE)
-{
-	if (id == null)
-	{
-		throw "slider element id is null";
-	}
-
-	if (parent == null)
-	{
-		throw "slider parent element is null";
-	}
-
-	if ((parent instanceof HTMLElement) == false)
-	{
-		throw "slider parent is not a HTMLElement";
-	}
-
-	const exists = document.getElementById(id);
-
-	if (exists != null)
-	{
-		throw `slider element with id "${id}" already exists`;
-	}
-
-	numVisible = parseInt(numVisible);
-
-	// ensure numVisible param is a number
-	if (isNaN(numVisible))
-	{
-		throw `numVisible param must be a number "${numVisible}"`;
-	}
-
-	if (numVisible <= 0)
-	{
-		throw `numVisible param must be greater than 0`;
-	}
-
-	const slider = document.createElement("div");
-
-	slider.id = id;
-	slider.classList.add("slider");
-
-	// left arrow
-	createArrow(slider, true);
-
-	// items container
-	const items = document.createElement("div");
-
-	items.id = `${slider.id}-items`;
-	items.classList.add("item-container");
-	items.dataset.visible = numVisible;
-
-	slider.appendChild(items);
-
-	// right arrow
-	createArrow(slider, false);
-
-	parent.appendChild(slider);
-
-	populate(id, values);
-}
-
-/**
- * Add/Subtract selected item index
- * @param {string} id slider element id
- * @param {number} amount amount to change
- */
-function change(id, amount)
-{
-	// don't accept 0
-	if (amount == 0)
-	{
-		return;
-	}
-
-	if (id == null)
-	{
-		throw "slider element id is null";
-	}
-
-	const slider = document.getElementById(id);
-
-	if (slider == null)
-	{
-		throw `slider element with id "${id}" doesn't exist`;
-	}
-
-	const items = slider.children[1];
-
-	if (items == null)
-	{
-		throw `slider element with id "${id}" doesn't have items container`;
-	}
-
-	const minIndex = parseInt(items.dataset.min);
-
-	if (isNaN(minIndex))
-	{
-		throw `slider element with id "${id}" doesn't have a valid min index`;
-	}
-
-	const maxIndex = parseInt(items.dataset.max);
-
-	if (isNaN(maxIndex))
-	{
-		throw `slider element with id "${id}" doesn't have a valid max index`;
-	}
-
-	let curIndex = parseInt(items.dataset.index);
-
-	if (isNaN(curIndex))
-	{
-		curIndex = minIndex;
-	}
-
-	const numItems = items.children.length;
-	const increment = Math.abs(amount) % numItems;
-	
-	let newIndex;
-	let newMinIndex = minIndex;
-	let newMaxIndex = maxIndex;
-
-	// calc new index
-	if (amount > 0)
-	{
-		newIndex = curIndex + increment;
-
-		// calc new max & min
-		if (newIndex > maxIndex)
-		{
-			newMaxIndex = newIndex;
-			newMinIndex = minIndex + (newMaxIndex - maxIndex);
-		}
-	}
-	else
-	{
-		newIndex = curIndex - increment;
-
-		if (newIndex < minIndex)
-		{
-			newMinIndex = newIndex;
-			newMaxIndex = maxIndex - (minIndex - newMinIndex);
-		}
-	}
-
-	// clamp index in between 0 and numItems
-	if (newIndex >= numItems)
-	{
-		newIndex -= numItems;
-		newMaxIndex -= numItems;
-		newMinIndex -= numItems;
-
-		// prevent min being negative
-		if (newMinIndex < 0)
-		{
-			newMaxIndex -= newMinIndex;
-			newMinIndex -= newMinIndex;
-		}
-	}
-	else if (newIndex < 0)
-	{
-		newIndex = numItems + newIndex;
-		newMinIndex = numItems + newMinIndex;
-		newMaxIndex = numItems + newMaxIndex;
-
-		// prevent max being greater than numItems
-		if (newMaxIndex >= numItems)
-		{
-			newMinIndex -= newMaxIndex - newIndex;
-			newMaxIndex = newIndex;
-		}
-	}
-	
-	let useDelay = false;
-
-	// change item visibility
-	if (minIndex != newMinIndex || maxIndex != newMaxIndex)
-	{
-		// hide visible items
-		for (let i = minIndex; i <= maxIndex; i++)
-		{
-			items.children[i].classList.remove("visible");
-		}
-
-		// show visible items
-		for (let i = newMinIndex; i <= newMaxIndex; i++)
-		{
-			items.children[i].classList.add("visible");
-		}
-
-		items.dataset.min = newMinIndex;
-		items.dataset.max = newMaxIndex;
-
-		useDelay = true;
-	}
-
-	setIndex(slider, newIndex, useDelay);
-}
-
