5d0b29d2e2627b5873477c2268cf029f5a388059
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-25 06:12:03 +0000

    feat(ui): registerCatalog func

--- ui/js/index.js
+++ ui/js/index.js
@@ -2,6 +2,45 @@ import Navbar from "./navbar.js";
 import Slider from "./slider.js";
 
 const messages = {};
+const catalogs = {};
+
+/*
+	Functions to be called from game script
+*/
+
+/**
+ * Register a catalog
+ * @param {object} payload Message payload
+ * @param {string} payload.id catalog id
+ * @param {object} payload.data catalog data
+ * @param {string[]} payload.data.categories list of catagories
+ * @param {object[][]} payload.data.vehicles list of vehicles
+ */
+messages.registerCatalog = function(payload)
+{
+	if (payload.id == null)
+	{
+		return;
+	}
+
+	if (payload.data == null)
+	{
+		return;
+	}
+
+	if (payload.data.categories == null || payload.data.categories.length == 0)
+	{
+		return;
+	}
+
+	if (payload.data.vehicles == null || payload.data.vehicles.length == 0)
+	{
+		return;
+	}
+
+	// store catalog
+	catalogs[payload.id] = payload.data;
+}
 
 /**
  * Listen for messages received from game script
