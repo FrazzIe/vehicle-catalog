708c921be24aa22bb05b0ac85871fef0867a49a0
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-25 23:45:49 +0000

    feat(ui/gamepad): further clean up, move into GamepadListener class

--- ui/js/gamepad.js
+++ ui/js/gamepad.js
@@ -1,181 +1,224 @@
-const useEvents = "ongamepadconnected" in window;
-const buttonThreshold = 0.12;
-const axesThreshold = 0.2;
+const INTERNAL_EVENTS_ENABLED = "ongamepadconnected" in window;
+const INTERNAL_CONNECTED_EVENT = "gamepadconnected";
+const INTERNAL_DISCONNECTED_EVENT = "gamepaddisconnected";
 
-let active = false;
-let gamepads = [];
+const DEFAULT_BUTTON_THRESHOLD = 0.12;
+const DEFAULT_AXES_THRESHOLD = 0.2;
 
-/**
- * Listen for gamepadconnected events
- * @param {Event} event 
- */
-function onGamepadConnected(event)
+class GamepadListener
 {
-	if (event.gamepad == null)
+	constructor()
 	{
-		return;
+		this.#gamepads = {};
+
+		this.#buttonThreshold = DEFAULT_BUTTON_THRESHOLD;
+		this.#axesThreshold = DEFAULT_AXES_THRESHOLD;
+
+		this.active = false;
 	}
 
-	if (event.gamepad.index == null)
+	set buttonThreshold(threshold)
 	{
-		return;
-	}
-
-	// store gamepad
-	gamepads[event.gamepad.index] = event.gamepad;
-}
-
-/**
- * Listen for gamepaddisconnected events
- * @param {Event} event 
- */
-function onGamepadDisconnected(event)
-{
-	// delete gamepad
-	delete gamepads[event.gamepad.index];
-}
-
-/**
- * Fetch a list of active gamepads from navigator
- * 
- * For browsers that don't support gamepad DOM events
- */
-function fetchGamepads() 
-{
-	let tempGamepads;
-
-	// reset gamepads global
-	gamepads = {};
-
-	// null check
-	if (window.navigator.getGamepads == null && window.navigator.webkitGetGamepads == null)
-	{
-		return;
-	}
-
-	// get gamepads list
-	if (window.navigator.getGamepads != null)
-	{
-		tempGamepads = window.navigator.getGamepads();
-	}
-	else
-	{
-		tempGamepads = window.navigator.webkitGetGamepads();
-	}
-
-	// store gamepads
-	for (let i = 0; i < tempGamepads.length; i++)
-	{
-		const gamepad = tempGamepads[i];
-
-		if (gamepad != null && gamepad.index != null)
+		if (isNaN(threshold))
 		{
-			gamepads[gamepad.index] = gamepad;
-		}
-	}
-}
-
-/**
- * Tick handler for animation frame
- * 
- * Queries each gamepad for button presses, dispatches events
- */
-function onTick()
-{
-	// use navigator fallback if gamepad events aren't supported
-	if (!useEvents)
-	{
-		fetchGamepads();
-	}
-
-	// loop through gamepads list
-	// query each gamepad
-	for (let i in gamepads)
-	{
-		const pad = gamepads[i];
-
-		// skip gamepads that have a "weird" configuration
-		if (pad.mapping != "standard")
-		{
-			continue;
+			throw "threshold is not a number";
 		}
 
-		// check buttons
-		for (let i = 0; i < pad.buttons.length; i++)
+		if (threshold < 0.0 || threshold > 1.0)
 		{
-			const value = pad.buttons[i].value ?? pad.buttons[i];
+			throw "threshold must be between 0.0 and 1.0";
+		}
 
-			if (value == null)
+		this.#buttonThreshold = threshold;
+	}
+
+	set axesThreshold(threshold)
+	{
+		if (isNaN(threshold))
+		{
+			throw "threshold is not a number";
+		}
+
+		if (threshold < 0.0 || threshold > 1.0)
+		{
+			throw "threshold must be between 0.0 and 1.0";
+		}
+
+		this.#buttonThreshold = threshold;
+	}
+
+	/**
+	 * Listen for gamepadconnected events
+	 * @param {Event} event 
+	 */
+	#connected(event)
+	{
+		if (event.gamepad == null)
+		{
+			return;
+		}
+	
+		if (event.gamepad.index == null)
+		{
+			return;
+		}
+	
+		// store gamepad
+		gamepads[event.gamepad.index] = event.gamepad;
+	}
+
+	/**
+	 * Listen for gamepaddisconnected events
+	 * @param {Event} event 
+	 */
+	#disconnected(event)
+	{
+		// delete gamepad
+		delete gamepads[event.gamepad.index];
+	}
+
+	/**
+	 * Fetch a list of active gamepads from navigator
+	 * 
+	 * For browsers that don't support gamepad DOM events
+	 */
+	#fetch() 
+	{
+		let tempGamepads;
+
+		// reset gamepads global
+		this.gamepads = {};
+
+		// null check
+		if (window.navigator.getGamepads == null && window.navigator.webkitGetGamepads == null)
+		{
+			return;
+		}
+
+		// get gamepads list
+		if (window.navigator.getGamepads != null)
+		{
+			tempGamepads = window.navigator.getGamepads();
+		}
+		else
+		{
+			tempGamepads = window.navigator.webkitGetGamepads();
+		}
+
+		// store gamepads
+		for (let i = 0; i < tempGamepads.length; i++)
+		{
+			const gamepad = tempGamepads[i];
+
+			if (gamepad != null && gamepad.index != null)
+			{
+				this.gamepads[gamepad.index] = gamepad;
+			}
+		}
+	}
+
+	/**
+	 * Tick handler for animation frame
+	 * 
+	 * Queries each gamepad for button presses, dispatches events
+	 */
+	#tickHandler()
+	{
+		// use navigator fallback if gamepad events aren't supported
+		if (!INTERNAL_EVENTS_ENABLED)
+		{
+			this.#fetch();
+		}
+
+		// loop through gamepads list
+		// query each gamepad
+		for (let i in this.gamepads)
+		{
+			const pad = this.gamepads[i];
+
+			// skip gamepads that have a "weird" configuration
+			if (pad.mapping != "standard")
 			{
 				continue;
 			}
 
-			if (value > buttonThreshold)
+			// check buttons
+			for (let i = 0; i < pad.buttons.length; i++)
 			{
-				// press
+				const value = pad.buttons[i].value ?? pad.buttons[i];
+
+				if (value == null)
+				{
+					continue;
+				}
+
+				if (value > buttonThreshold)
+				{
+					// press
+				}
+				else
+				{
+					// release
+				}
 			}
-			else
+
+			// check thumbsticks
+			for (let i = 0; i < pad.axes.length; i++)
 			{
-				// release
-			}
+				const value = pad.axes[i];
+
+				if (value == null)
+				{
+					continue;
+				}
+
+				if (value > axesThreshold || value < -axesThreshold)
+				{
+					// active
+				}
+			}		
 		}
 
-		// check thumbsticks
-		for (let i = 0; i < pad.axes.length; i++)
+		if (this.active)
 		{
-			const value = pad.axes[i];
-
-			if (value == null)
-			{
-				continue;
-			}
-
-			if (value > axesThreshold || value < -axesThreshold)
-			{
-				// active
-			}
-		}		
+			requestAnimationFrame(this.#tickHandler);
+		}
 	}
 
-	if (active)
+	/**
+	 * Start listening for Gamepad events
+	 */
+	start()
 	{
-		requestAnimationFrame(onTick);
+		if (this.active)
+		{
+			return;
+		}
+	
+		if (INTERNAL_EVENTS_ENABLED)
+		{
+			window.addEventListener(INTERNAL_CONNECTED_EVENT, this.#connected);
+			window.addEventListener(INTERNAL_DISCONNECTED_EVENT, this.#disconnected);
+		}
+	
+		this.active = true;
+	
+		requestAnimationFrame(this.#tickHandler);
+	}
+
+	/**
+	 * Stop listening for Gamepad events
+	 */
+	stop()
+	{
+		this.active = false;
+
+		if (INTERNAL_EVENTS_ENABLED)
+		{
+			window.removeEventListener(INTERNAL_CONNECTED_EVENT, this.#connected);
+			window.removeEventListener(INTERNAL_DISCONNECTED_EVENT, this.#disconnected);
+		}
 	}
 }
 
-/**
- * Start gamepad tick handler
- */
-function startGamepadListener()
-{
-	if (active)
-	{
-		return;
-	}
-
-	if (useEvents)
-	{
-		window.addEventListener("gamepadconnected", onGamepadConnected, false);
-		window.addEventListener("gamepaddisconnected", onGamepadDisconnected, false);
-	}
-
-	active = true;
-
-	requestAnimationFrame(onTick);
-}
-
-/**
- * Stop gamepad tick handler
- */
-function stopGamepadListener()
-{
-	active = false;
-
-	if (useEvents)
-	{
-		window.removeEventListener("gamepadconnected", onGamepadConnected);
-		window.removeEventListener("gamepaddisconnected", onGamepadDisconnected);
-	}
-}
-
-export { startGamepadListener, stopGamepadListener };
+export default new GamepadListener();
