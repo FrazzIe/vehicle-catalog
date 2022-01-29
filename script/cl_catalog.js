5f32d6c79034b50d3fc9805bf220997e33717562
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-29 17:43:59 +0000

    feat(script/catalog): response handler func
    Synchronises registered catalogs with server

--- script/cl_catalog.js
+++ script/cl_catalog.js
@@ -6,4 +6,27 @@ function onInit()
 
 }
 
+/**
+ * Listen for init response from server
+ * 
+ * Synchronises registered catalogs with server
+ * 
+ * @param {object[]} catalogs collection of catalog objects
+ */
+function onInitResponse(catalogs)
+{
+	// register each catalog
+	for (const id in catalogs)
+	{
+		// send catalog to web ui
+		SendNUIMessage({
+			type = "registerCatalog",
+			payload = {
+				id: id,
+				data: catalogs[id]
+			}
+		});
+	}
+}
+
 on("onClientResourceStart", onInit);
