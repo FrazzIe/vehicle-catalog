01765a1c1f6bbd8a5704a5d63849f71be1149933
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-29 23:33:29 +0000

    feat(script/preview): createPreview func
    Create a local vehicle

--- script/cl_preview.js
+++ script/cl_preview.js
@@ -3,6 +3,28 @@ const entities = [];
 let curPreview;
 
 /**
+ * Create a local vehicle
+ * @param {string} model vehicle model
+ * @param {object} position vehicle coordinates
+ * @param {number} position.x x
+ * @param {number} position.y y
+ * @param {number} position.z z
+ * @param {number} position.heading heading
+ * @returns {number} vehicle handle
+ */
+function createPreview(model, position)
+{
+	const handle = CreateVehicle(model, position.x, position.y, position.z, position.heading, false, false);
+	
+	SetVehicleOnGroundProperly(handle);
+	FreezeEntityPosition(handle, true);
+	SetEntityCollision(handle, false, true);
+	SetModelAsNoLongerNeeded(model);
+
+	return handle;
+}
+
+/**
  * Event listener for setActiveVehicle
  * @param {string} data vehicle model
  * @param {function} callback 
