3bb144e35d22a47fed6183f5368897f9738afd0c
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-29 10:41:02 +0000

    feat(ui/widgets): create widgets func

--- ui/js/vehicleWidgets.js
+++ ui/js/vehicleWidgets.js
@@ -2,6 +2,7 @@ import Stats from "./stats.js";
 
 let label;
 let stats;
+let price;
 let button;
 
 /**
@@ -10,7 +11,60 @@ let button;
  */
 function create()
 {
-	// TODO
+	const widgets = document.createElement("div");
+
+	widgets.id = "vehicle-widgets";
+
+	// label widget
+	label = document.createElement("div");
+
+	label.id = `${widgets.id}-label`;
+	label.classList.add("widget", "label");
+	label.textContent = "VEHICLE_NAME_PLACEHOLDER";
+	
+	widgets.appendChild(label);
+
+	// stats + price container
+	const container = document.createElement("div");
+
+	container.classList.add("widget", "bottom", "transparent", "gap");
+
+	// stats widget
+	const statsContainer = document.createElement("div");
+
+	statsContainer.classList.add("widget", "stats");
+
+	const statsLabel = document.createElement("div");
+
+	statsLabel.textContent = "Stats";
+
+	statsContainer.appendChild(statsLabel);
+
+	stats = new Stats(`${widgets.id}-stats`, [ "FMMC_VEHST_0", "FMMC_VEHST_1", "FMMC_VEHST_2", "FMMC_VEHST_3" ]);
+
+	statsContainer.appendChild(stats.domElement);
+
+	container.appendChild(statsContainer);
+
+	// price widget
+	price = document.createElement("div");
+
+	price.id = `${widgets.id}-price`;
+	price.classList.add("widget", "price", "transparent");
+
+	container.appendChild(price);
+
+	widgets.appendChild(container);
+
+	// select-btn widget
+	button = document.createElement("button");
+
+	button.id = `${widgets.id}-select-btn`;
+	button.classList.add("widget", "bottom", "right", "select-btn");
+
+	widgets.appendChild(button);
+
+	return widgets;
 }
 
-export { label, stats, button, create };
+export { label, stats, price, button, create };
