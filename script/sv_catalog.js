9ce220014d5a035d5ffb960a86059e2801b9e721
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-29 16:14:49 +0000

    feat(script/catalog): onRegisterCatalog event handler

---
+++ script/sv_catalog.js
@@ -0,0 +1,38 @@
+const catalogs = {};
+
+/**
+ * Registers a catalog
+ * @param {string} id catalog id
+ * @param {object} data catalog data
+ * @param {string[]} data.categories list of catagories
+ * @param {object[][]} data.vehicles list of vehicles
+ */
+function onRegisterCatalog(id, data)
+{
+	if (id == null || id == "")
+	{
+		throw `Unable to register catalog, malformed id: "${id}"`;
+	}
+
+	if (data == null)
+	{
+		throw `Unable to register catalog, data is null`;
+	}
+
+	if (data.categories == null || data.categories.length == 0)
+	{
+		throw "Unable to register catalog, data.categories is null or empty";
+	}
+
+	if (data.vehicles == null || data.vehicles.length == 0)
+	{
+		throw "Unable to register catalog, data.vehicles is null or empty";
+	}
+
+	// store catalog
+	catalogs[id] = data;
+
+	emitNet("vehicle-catalog:registerCatalog", -1, id, data);
+}
+
+on("vehicle-catalog:registerCatalog", onRegisterCatalog);
