3dc0d20a7914def5e501241d290a13e302d4ca02
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-29 21:10:50 +0000

    feat(script/catalog): use onRegisterCatalog, rem dupe code

--- script/cl_catalog.js
+++ script/cl_catalog.js
@@ -27,35 +27,7 @@ function onInitResponse(catalogs)
 	// register each catalog
 	for (const id in catalogs)
 	{
-		const catalog = catalogs[id];
-
-		if (catalog == null)
-		{
-			continue;
-		}
-
-		// get resolved vehicle label for each vehicle
-		for (let i = 0; i < catalog.vehicles.length; i++)
-		{
-			for (let j = 0; j < catalog.vehicles[i]; j++)
-			{
-				const vehicle = catalog.vehicles[i][j];
-
-				if (vehicle != null)
-				{
-					vehicle.label = getVehicleName(vehicle.model);
-				}
-			}
-		}
-
-		// send catalog to web ui
-		SendNUIMessage({
-			type = "registerCatalog",
-			payload = {
-				id: id,
-				data: catalogs[id]
-			}
-		});
+		onRegisterCategory(id, catalogs[data]);
 	}
 
 	removeEventListener("vehicle-catalog:init", this);
