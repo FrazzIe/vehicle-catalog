e67b8bc100f300f8fe822579b34c5c7e4b695160
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-30 15:49:53 +0000

    feat(script/preview): rem createPreview, curPreview

--- script/cl_preview.js
+++ script/cl_preview.js
@@ -1,29 +1,5 @@
 const entities = [];
 
-let curPreview;
-
-/**
- * Create a local vehicle
- * @param {string} model vehicle model
- * @param {object} position vehicle coordinates
- * @param {number} position.x x
- * @param {number} position.y y
- * @param {number} position.z z
- * @param {number} position.heading heading
- * @returns {number} vehicle handle
- */
-function createPreview(model, position)
-{
-	const handle = CreateVehicle(model, position.x, position.y, position.z, position.heading, false, false);
-	
-	SetVehicleOnGroundProperly(handle);
-	FreezeEntityPosition(handle, true);
-	SetEntityCollision(handle, false, true);
-	SetModelAsNoLongerNeeded(model);
-
-	return handle;
-}
-
 /**
  * Event listener for setActiveVehicle
  * @param {string} data vehicle model
@@ -31,24 +7,38 @@ function createPreview(model, position)
  */
 function onSetActiveVehicle(data, callback)
 {
+	const model = data;
+
 	if (activeCatalog == null)
 	{
 		return;
 	}
 
-	activeCatalog.model = data;
+	activeCatalog.model = model;
 
 	// delete previous vehicles
 	while (entities.length > 0)
 	{
 		DeleteEntity(entities.pop());
 	}
+	
+	// TODO: Load model
+	
+	if (activeCatalog.position == null)
+	{
+		return;
+	}
 
-	// TODO: create vehicle
-	curPreview = null;
+	// create vehicle
+	const handle = CreateVehicle(model, activeCatalog.position.x, activeCatalog.position.y, activeCatalog.position.z, activeCatalog.position.heading, false, false);
 
-	// add curPreview entity list
-	entities.push(curPreview);
+	SetVehicleOnGroundProperly(handle);
+	FreezeEntityPosition(handle, true);
+	SetEntityCollision(handle, false, true);
+	SetModelAsNoLongerNeeded(model);
+
+	// add created vehicle to entity list
+	entities.push(handle);
 }
 
 on("__cfx_nui:setActiveVehicle", onSetActiveVehicle);
