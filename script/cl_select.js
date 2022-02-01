90b0b48928f3beef42c6a635f325849e97f744ec
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-02-01 10:30:13 +0000

    feat(script/select): store callback, send to server

--- script/cl_select.js
+++ script/cl_select.js
@@ -1,16 +1,26 @@
+let selectCallback;
+
 /**
+ * Event listener for select vehicle
+ * 
+ * Starts vehicle selected transaction
  * 
  * @param {object} data
- * @param {string} data.id catalog id
+ * @param {string} data.catalog catalog id
  * @param {number} data.category category index
  * @param {number} data.vehicle vehicle index
  * @param {function} callback 
  */
 function onSelectVehicle(data, callback)
 {
+	selectCallback = callback;
+
+	emitNet("vehicle-catalog:selectStart", data);
+}
 
 }
 
 RegisterNuiCallbackType("select");
 
 on("__cfx_nui:select", onSelectVehicle);
+onNet("vehicle-catalog:selectEnd", onSelectEnd);
